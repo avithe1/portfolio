1164aa90ca07196ad6f944221d24be0aa3b0c8b9
Avinash <avithe1@gmail.com>
2025-09-14 15:49:12 +0530

    myportfolio

---
+++ app/data.ts
@@ -0,0 +1,44 @@
+export const websiteData = {
+  projects: [
+    {
+      id: 1,
+      title: "Dribble search clone",
+      link: "https://dribble-search.vercel.app/",
+    },
+    {
+      id: 2,
+      title: "Superfan Website",
+      link: "https://superfansite.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "Smokespace",
+      link: "https://smokespace.com/",
+    },
+    {
+      id: 4,
+      title: "Tubeseek",
+      link: "https://tubeseek.vercel.app/",
+    },
+    {
+      id: 5,
+      title: "Nutraverse Website",
+      link: "https://nutraverse-website.vercel.app/",
+    },
+    {
+      id: 6,
+      title: "JustNod",
+      link: "https://justnodapp.vercel.app/",
+    },
+    {
+      id: 7,
+      title: "HighwayCalling",
+      link: "https://highwaycalling.com/",
+    },
+  ],
+  myexpertise: {
+    skills: ["HTML5", "CSS3/Tailwind", "React", "Next.JS", "Typescript"],
+    backend: ["Firebase", "Node.JS", "SQL"],
+    tools: ["Git", "Vercel"],
+  },
+};
